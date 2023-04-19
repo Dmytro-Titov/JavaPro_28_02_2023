76e7d0a0adba3badc4bba4cb899bcfacc1237f34
Dmytro Titov <octopusless@gmail.com>
2023-04-19 12:33:00 +0300

    Complete hw11

--- src/main/java/com/hillel/titov/lessons/lesson14/hw10/BookReadingException.java
+++
@@ -1,7 +0,0 @@
-package com.hillel.titov.lessons.lesson14.hw10;
-
-public class BookReadingException extends Exception{
-    public BookReadingException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/hillel/titov/lessons/lesson14/hw10/WriteStatisticException.java
+++
@@ -1,7 +0,0 @@
-package com.hillel.titov.lessons.lesson14.hw10;
-
-public class WriteStatisticException extends Exception{
-    public WriteStatisticException(String message) {
-        super(message);
-    }
-}
