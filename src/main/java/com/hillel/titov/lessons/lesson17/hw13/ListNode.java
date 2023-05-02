421fadc4b7cd759098f50db18c4bb4c9c062f9db
Dmytro Titov <octopusless@gmail.com>
2023-05-02 10:49:00 +0300

    Complete hw13

---
+++ src/main/java/com/hillel/titov/lessons/lesson17/hw13/ListNode.java
@@ -0,0 +1,19 @@
+package com.hillel.titov.lessons.lesson17.hw13;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
